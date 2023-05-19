e2b363679d13f291434adf46edf59b11388054ed
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-05-19 20:03:51 -0300

    first commit

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -5,28 +5,26 @@ const About = () => {
   return (
     <section className="about">
       <div className="container">
-        <div className="aboutContent">
-          <div className="imageSide">
-            <img className="mainImg" src={notebook}></img>
-          </div>
-          <div>
-            <h4>About me</h4>
-            <h3>
-              A dedicated Front-end Developer
-              <br />
-              based in Florianópolis, Brazil 📍
-            </h3>
-            <p>
-              As a Junior Front-End Developer, I possess an impressive arsenal
-              of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I
-              excel in designing and maintaining responsive websites that offer
-              a smooth user experience. My expertise lies in crafting dynamic,
-              engaging interfaces through writing clean and optimized code and
-              utilizing cutting-edge development tools and techniques. I am also
-              a team player who thrives in collaborating with cross-functional
-              teams to produce outstanding web applications.
-            </p>
-          </div>
+        <div className="imageSide">
+          <img className="mainImg" src={notebook}></img>
+        </div>
+        <div className="textSide">
+          <h4>About me</h4>
+          <h3>
+            A dedicated Front-end Developer
+            <br />
+            based in Florianópolis, Brazil 📍
+          </h3>
+          <p>
+            As a Junior Front-End Developer, I possess an impressive arsenal of
+            skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel
+            in designing and maintaining responsive websites that offer a smooth
+            user experience. My expertise lies in crafting dynamic, engaging
+            interfaces through writing clean and optimized code and utilizing
+            cutting-edge development tools and techniques. I am also a team
+            player who thrives in collaborating with cross-functional teams to
+            produce outstanding web applications.
+          </p>
         </div>
       </div>
     </section>
