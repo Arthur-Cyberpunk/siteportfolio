d7db63206081f88d7df34b4a9320437e71558e0a
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-05-19 18:27:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import "./styles/globals.scss";
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
